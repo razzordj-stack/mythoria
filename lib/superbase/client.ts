064439158b0c7b4b0364daa116caeee8b6f965e4
razzordj-stack <razzordj@googlemail.com>
2026-07-29 01:23:16 +0200

    feat: save characters to Supabase

--- lib/superbase/client.ts
+++ lib/superbase/client.ts
@@ -1,20 +1,26 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    const supabaseUrl =
+        process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+    const supabaseKey =
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
     if (!supabaseUrl) {
         throw new Error(
-            "NEXT_PUBLIC_SUPABASE_URL fehlt in der Datei .env.local.",
+            "NEXT_PUBLIC_SUPABASE_URL fehlt in den Umgebungsvariablen.",
         );
     }
 
     if (!supabaseKey) {
         throw new Error(
-            "NEXT_PUBLIC_SUPABASE_ANON_KEY fehlt in der Datei .env.local.",
+            "NEXT_PUBLIC_SUPABASE_ANON_KEY fehlt in den Umgebungsvariablen.",
         );
     }
 
-    return createBrowserClient(supabaseUrl, supabaseKey);
+    return createBrowserClient(
+        supabaseUrl,
+        supabaseKey,
+    );
 }
